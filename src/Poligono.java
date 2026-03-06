4933e279dc7e39be6ae08762af4e438725ad0303
zDefcon <antholozadarequelme@gmail.com>
2026-03-06 02:11:40 -0500

    Agrega método abstracto area() en la clase Poligono
    Este cambio define un método abstracto que obliga a las clases hijas, como Triangulo y Rectangulo, a implementar su propia versión del método para calcular el área correspondiente del polígono.

--- src/Poligono.java
+++ src/Poligono.java
@@ -15,5 +15,7 @@
         return "\nNumero Lados = " + numLados + "\n";
     }
 
+    // el metodo abstract me permite definir un metodo sin implementacion en la clase padre, y obligar a las clases hijas a implementar ese metodo. En este caso, el metodo area() es abstracto, y cada clase hija (Triangulo, Rectangulo) debe implementar su propia version del metodo area() para calcular el area del poligono correspondiente.
+
     public abstract double area(); // metodo abstracto para calcular el area del poligono
 }
