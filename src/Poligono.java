7950f0e68ebb11077a332bc8eb0ae2cedbe7b6df
zDefcon <antholozadarequelme@gmail.com>
2026-03-06 02:00:30 -0500

    Agrega clase abstracta Poligono con atributos y métodos básicos
    Se crea la clase abstracta Poligono que incluye un atributo protegido para almacenar el número de lados, un constructor, un método para obtener el número de lados y un método abstracto para calcular el área. También se implementa un método toString para representar el polígono.

---
+++ src/Poligono.java
@@ -0,0 +1,19 @@
+﻿public abstract class  Poligono {
+
+    protected int numLados; // atributo protegido que almacena el numero de lados del poligono
+
+    public Poligono(int numLados){ // metodo constructor que recibe el numero de lados del poligono
+        this.numLados = numLados;
+    }
+
+    public int getNumLados(){
+        return numLados;
+    }
+
+    @Override
+    public String toString(){
+        return "\nPoligono con " + numLados + " lados.\n";
+    }
+
+    public abstract double area(); // metodo abstracto para calcular el area del poligono
+}
